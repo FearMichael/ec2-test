3d8464b60bed33addc1c0667bc4c6440b538f6af
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2020-04-18 12:32:25 -0400

    config

--- config/config.js
+++ config/config.js
@@ -8,18 +8,18 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": "test",
+    "host": process.env.DB_HOST,
     "port": 3306,
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": "test",
+    "host": process.env.DB_HOST,
     "port": 3306,
     "dialect": "mysql"
   }
